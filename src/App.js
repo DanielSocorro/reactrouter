8b04673a585523eb630126d2833ef0b79c968ae4
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-02-07 13:10:15 -0300

    added fake login authentication

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { HomePage } from "./HomePage";
 import { BlogPage } from "./BlogPage";
 import { BlogPost } from "./BlogPost";
 import { ProfilePage } from "./ProfilePage";
+import { LoginPage } from "./LoginPage";
+import { LogoutPage } from "./Logout";
+
  
 function App() {
   return (
@@ -13,8 +16,13 @@ function App() {
 
       <Routes>
         <Route path='/' element={<HomePage />}/>
-        <Route path='/blog' element={<BlogPage />}/>
-        <Route path='/blog/:slug' element={<BlogPost />}/>
+        
+        <Route path='/blog' element={<BlogPage />}>
+        <Route path=':slug' element={<BlogPost />} />
+        </Route>
+
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/logout' element={<LogoutPage />}/>
         <Route path='/profile' element={<ProfilePage />}/>
         <Route path='*' element={<p>Not Found</p>}/>
       </Routes>     
---
+++ src/Logout.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function LogoutPage() {
+    return (
+        <h1>LogoutPage</h1>
+    );
+}
+
+export { LogoutPage };
